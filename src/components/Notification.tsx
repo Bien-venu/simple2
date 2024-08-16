81c2e7b81f40827ca129f17270b8d9da8006a82f
Bien-venu <iambienvenu@gmail.com>
2024-08-16 11:43:00 +0300

    change some

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -7,7 +7,8 @@ const Notification = ({ a }: any) => {
     <div className="h-full flex-1 overflow-auto p-8">
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-4">
-          <h1 className="text-2xl font-semibold">{a.task}</h1>
+          <h1 className="text-2xl font-semibold">{a.name}</h1>
+          <h4 className="text-xl font-semibold">{a.task}</h4>
           <p className="flex flex-col items-start gap-1 text-xs text-grey">
             Sub-issue of <span className="uppercase font-light">{a.team}</span>
             <span className=" font-normal leading-3 tracking-normal text-sm"> {a.about}</span>
